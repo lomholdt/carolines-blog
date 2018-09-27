a16daecfdf608750548c79a39836d9237638d1ba
lomholdt <jonaslomholdt@gmail.com>
2018-09-27 20:35:20 +0200

    Update packages

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default class IndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Carolines Blog</h1>
+              <h1 className="is-size-1 has-text-weight-bold has-text-centered">Carolines Blog</h1>
             </div>
             {posts
               .map(({ node: post }) => (
